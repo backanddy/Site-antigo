8fcaf8f441b87bc5e71d151e342d44b82153ddb8
Andressa Neves <andressaneves.dev@gmail.com>
2020-01-03 23:42:37 -0300

    [add]Habilidades animada com D3 e Timeline

---
+++ js/macumba/skills-data.js
@@ -0,0 +1,24 @@
+let data = [{
+  cat: 'library',
+  name: 'HTML',
+  value: 100,
+  icon: 'img/html5-css3.png',
+  desc: `
+				D3.js (or just D3 for Data-Driven Documents) is a JavaScript library for
+				producing dynamic, interactive data visualizations in web browsers.
+				It makes use of the widely implemented SVG, HTML5, and CSS standards.<br>
+				This infographic you are viewing is made with D3.
+			`
+},
+{
+  cat: 'library',
+  name: 'D3',
+  value: 20,
+  icon: 'img/d3.svg',
+  desc: `
+				D3.js (or just D3 for Data-Driven Documents) is a JavaScript library for
+				producing dynamic, interactive data visualizations in web browsers.
+				It makes use of the widely implemented SVG, HTML5, and CSS standards.<br>
+				This infographic you are viewing is made with D3.
+			`
+}];
